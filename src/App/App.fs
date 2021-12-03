abcb12c05cf284db04695062f5d9846a06a9d163
Martin Grotz <martin.grotz@mathema.de>
2021-12-03 16:49:14 +0100

    second increment: add steps

--- src/App/App.fs
+++ src/App/App.fs
@@ -6,21 +6,47 @@ open Sutil.Attr
 open Sutil.Helpers
 
 let idGenerator = makeIdGenerator ()
-let ticksPerMinute = System.TimeSpan.TicksPerMinute
 
+type Sound =
+    | SingleShortBell
+    | SingleLongBell
+type TimerStep = {
+    StepId : int
+    Minutes : int
+    Sound : Sound
+}
+let createStep minutes sound = 
+    {StepId = idGenerator(); Minutes = minutes; Sound = sound;}
 
-type Model = { LastTimerValue : int  }
+type Model = { 
+    LastTimerValue : int  
+    TimerStepsReversed : TimerStep list
+    }
 let getLastTimerValue m = m.LastTimerValue
+let getTimerSteps m = m.TimerStepsReversed |> List.rev
 
 type Message =
     | LastTimerValueChanged of int
+    | AddSingleShortBell
+    | AddSingleLongBell
 
 
-let init () : Model= ({ LastTimerValue = 0 })
+let init () : Model= ({ 
+    LastTimerValue = 1 
+    TimerStepsReversed = []
+    })
 
 let update (msg : Message) (model : Model) : Model =
     match msg with
     | LastTimerValueChanged newValue -> { model with LastTimerValue = newValue }
+    | AddSingleShortBell -> 
+        let withNewStep = (createStep model.LastTimerValue SingleShortBell) :: model.TimerStepsReversed
+        Browser.Dom.console.info(Fable.Core.JS.JSON.stringify withNewStep)
+
+        { model with TimerStepsReversed = withNewStep }
+    | AddSingleLongBell -> 
+        let withNewStep = (createStep model.LastTimerValue SingleLongBell) :: model.TimerStepsReversed
+        { model with TimerStepsReversed = withNewStep }
 
 let planEditView (model: IStore<Model>) dispatch = 
     Html.ul [
@@ -34,6 +60,20 @@ let planEditView (model: IStore<Model>) dispatch =
                           Attr.placeholder "How many minutes?"
                       ]
       ]
+
+      Html.li [
+        Html.button [
+            type' "button"
+            onClick (fun _ -> dispatch AddSingleShortBell) []
+            Html.text "Add single short bell"
+        ]
+
+        Html.button [
+            type' "button"
+            onClick (fun _ -> dispatch AddSingleLongBell) []
+            Html.text "Add single long bell"
+        ]
+    ]   
   ]    
 
 let view() =
